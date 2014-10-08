88bee3106fda52cf2453ebdb0fe9b665f3fce13e
Magnus Stenman <mst1028@hotmail.com>
2014-10-08 12:52:21 +0200

    faceData created

--- FaceRecognition/src/FaceData.java
+++ FaceRecognition/src/FaceData.java
@@ -1,4 +1,19 @@
 
 public class FaceData {
 
+    private String imageID;
+    private Integer[][] faceData;
+
+    public FaceData(String imageID, Integer[][] faceData) {
+        this.imageID = imageID;
+        this.faceData = faceData;
+    }
+
+    public String getImageID() {
+        return imageID;
+    }
+
+    public Integer[][] getFaceData() {
+        return faceData;
+    }
 }
