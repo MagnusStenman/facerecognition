81052d0facec029de1cb7c05e3a4882f1ac87835
christer <jakobsson.christer@gmail.com>
2014-10-08 10:52:03 +0200

    fedfd

--- FaceRecognition/src/Faces.java
+++ FaceRecognition/src/Faces.java
@@ -1,11 +1,21 @@
 public class Faces {
 
+	public Faces(FileReader fr) {
+
+		
+		
+	}
+
 	public static void main(String[] args) {
-		if (args.length > 0) {
-			System.out.println("FILE " + args[0]);
-		} else {
-			
+		FileReader fr;
+		if (args.length > 2) {
+			fr = new FileReader(args[0], args[1], args[2]);
+			Faces face = new Faces(fr);
+		} else {  
+			System.err.println("Not enough arguments to run program.");
 		}
 	}
+	
+	
 
 }
